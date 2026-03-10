decf1969b10e810d398555a74089e7861237a7a0
ATOMPassakorn <atom15440@gmail.com>
2026-03-10 18:55:03 +0700

    แก้ไข Start_server.bat
    - ให้ call npm install มาก่อนเปิด server กันการไม่มี dependencies

--- Start_server.bat
+++ Start_server.bat
@@ -4,8 +4,13 @@ echo ====================================================
 echo Starting EASYMANAGE Warehouse Management System...
 echo ====================================================
 
+echo Installing dependencies (if missing)...
+call npm install
+
+echo Opening Web Browser...
 start http://localhost:3000
 
+echo Starting Node.js Server...
 npm run dev
 
 pause
